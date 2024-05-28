b5103150d040b6767f68161aa3184bf390351bc4
Florian Wartner <florian.wartner@cloud37.io>
2024-05-28 16:49:15 +0200

    WIP

--- custom_components/openwakeword-installer/config_flow.py
+++ custom_components/openwakeword-installer/config_flow.py
@@ -1,18 +1,40 @@
-import voluptuous as vol
 from homeassistant import config_entries
-from .const import DOMAIN, CONF_REPOSITORY_URL, CONF_FOLDER_PATH
+from homeassistant.core import callback
+import voluptuous as vol
+
+from .const import DOMAIN
+
+@callback
+def configured_instances(hass):
+    return [entry.data["repository_url"] for entry in hass.config_entries.async_entries(DOMAIN)]
 
 class OpenWakeWordConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Handle a config flow for OpenWakeWord."""
+
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
+        """Handle the initial step."""
+        errors = {}
         if user_input is not None:
-            return self.async_create_entry(title="OpenWakeWord", data=user_input)
+            if user_input["repository_url"] in configured_instances(self.hass):
+                errors["base"] = "already_configured"
+            else:
+                # Validate the URL
+                # Add your validation code here
+                return self.async_create_entry(title="OpenWakeWord", data=user_input)
+
+        data_schema = vol.Schema({
+            vol.Required("repository_url"): str,
+            vol.Optional("folder_path", default=""): str,
+        })
 
         return self.async_show_form(
             step_id="user",
-            data_schema=vol.Schema({
-                vol.Required(CONF_REPOSITORY_URL): str,
-                vol.Optional(CONF_FOLDER_PATH, default=""): str,
-            })
+            data_schema=data_schema,
+            errors=errors,
         )
+
+    async def async_step_import(self, user_input=None):
+        """Handle the import from configuration.yaml."""
+        return await self.async_step_user(user_input)
--- custom_components/openwakeword-installer/sensor.py
+++ custom_components/openwakeword-installer/sensor.py
@@ -1,57 +1,39 @@
-import logging
-from datetime import timedelta
 from homeassistant.helpers.entity import Entity
-from homeassistant.helpers.event import async_track_time_interval
-from .const import DOMAIN, DEFAULT_SCAN_INTERVAL
-from .update import check_for_updates
 
-_LOGGER = logging.getLogger(__name__)
+from .const import DOMAIN
 
 async def async_setup_entry(hass, config_entry, async_add_entities):
-    repository_url = config_entry.data["repository_url"]
-    folder_path = config_entry.data.get("folder_path", "")
-
-    sensor = OpenWakeWordSensor(hass, repository_url, folder_path)
-    async_add_entities([sensor], update_before_add=True)
-
-    async def async_update_data(_):
-        await sensor.async_update()
-
-    async_track_time_interval(hass, async_update_data, timedelta(seconds=DEFAULT_SCAN_INTERVAL))
-    return True
+    """Set up the OpenWakeWord sensor."""
+    async_add_entities([OpenWakeWordSensor(hass, config_entry.data)], True)
 
 class OpenWakeWordSensor(Entity):
-    def __init__(self, hass, repository_url, folder_path):
-        self._hass = hass
+    """Representation of a Sensor."""
+
+    def __init__(self, hass, config):
+        """Initialize the sensor."""
+        self.hass = hass
         self._state = None
-        self._repository_url = repository_url
-        self._folder_path = folder_path
-        self._attr_name = "OpenWakeWord Update Status"
-        self._attr_unique_id = f"openwakeword_{repository_url}"
         self._last_update = None
 
     @property
     def name(self):
-        return self._attr_name
+        """Return the name of the sensor."""
+        return "OpenWakeWord Update Status"
 
     @property
     def state(self):
+        """Return the state of the sensor."""
         return self._state
 
     @property
-    def unique_id(self):
-        return self._attr_unique_id
-
-    @property
     def extra_state_attributes(self):
+        """Return the state attributes."""
         return {
             "last_update": self._last_update
         }
 
-    async def async_update(self):
-        _LOGGER.debug("Checking for updates")
-        new_files, last_update = await self._hass.async_add_executor_job(
-            check_for_updates, self._repository_url, self._folder_path
-        )
-        self._state = "updated" if new_files else "no_update"
-        self._last_update = last_update
+    def update(self):
+        """Fetch new state data for the sensor."""
+        # Logic to update sensor state
+        self._state = "idle"  # Example state
+        self._last_update = "2023-05-27T12:34:56"  # Example last update time
